717eeaf53e77936c02ee60d87dfe84f89a8fcc88
Aliaksandr Silchanka <cinches_sanctum0p@icloud.com>
2025-02-10 20:36:10 +0100

    refactor: make ErrorButton functional component

--- src/components/ErrorButton/ErrorButton.tsx
+++ src/components/ErrorButton/ErrorButton.tsx
@@ -1,33 +1,18 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import './ErrorButton.css';
 
-type ErrorButtonState = { throwError: boolean };
+const ErrorButton = () => {
+  const [throwError, setThrowError] = useState(false);
 
-class ErrorButton extends Component<object, ErrorButtonState> {
-  constructor(props: object) {
-    super(props);
-
-    this.state = {
-      throwError: false,
-    };
+  if (throwError) {
+    throw new Error('You broke the app! Are you satisfied now?');
   }
 
-  render() {
-    if (this.state.throwError) {
-      throw new Error('You broke the app! Are you satisfied now?');
-    }
-
-    return (
-      <button
-        className="error-button"
-        onClick={() => {
-          this.setState({ throwError: true });
-        }}
-      >
-        Throw error!
-      </button>
-    );
-  }
-}
+  return (
+    <button className="error-button" onClick={() => setThrowError(true)}>
+      Throw error!
+    </button>
+  );
+};
 
 export default ErrorButton;
