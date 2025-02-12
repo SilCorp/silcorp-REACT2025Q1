9f629035cbcbe846bdb6d2a8dc95209ac99fd801
Aliaksandr Silchanka <cinches_sanctum0p@icloud.com>
2025-02-12 19:06:28 +0100

    refactor: make App functional component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import { Component } from 'react';
 import SearchInput from './components/SearchInput/SearchInput.tsx';
 import './App.css';
 import SearchResult from './components/SearchResult/SearchResult.tsx';
 import ErrorButton from './components/ErrorButton/ErrorButton.tsx';
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <header>
-          <h1 className="title">Pokemon finder</h1>
-          <SearchInput />
-        </header>
-        <main>
-          <SearchResult />
-        </main>
-        <footer>
-          <ErrorButton />
-        </footer>
-      </>
-    );
-  }
-}
+const App = () => {
+  return (
+    <>
+      <header>
+        <h1 className="title">Pokemon finder</h1>
+        <SearchInput />
+      </header>
+      <main>
+        <SearchResult />
+      </main>
+      <footer>
+        <ErrorButton />
+      </footer>
+    </>
+  );
+};
 
 export default App;
